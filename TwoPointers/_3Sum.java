b8da5acd969b4a71839d15ab4a4360783a2dc108
Raman Ailawadhi <ramanailawadhi@gmail.com>
2025-04-14 22:23:39 +0530

    3 Sum

---
+++ TwoPointers/_3Sum.java
@@ -0,0 +1,44 @@
+package TwoPointers;
+import java.util.*;
+
+public class _3Sum {
+    public List<List<Integer>> threeSum(int[] nums) {
+        List<List<Integer>> ans = new ArrayList<>();
+
+        Arrays.sort(nums);
+
+        int base = 0;
+
+        while(base < nums.length-2) {
+            int left = base + 1;
+            int right = nums.length-1;
+            int required = -nums[base];
+
+            while(left < right) {
+                if(nums[left] + nums[right] == required) {
+                    ans.add(Arrays.asList(nums[base],nums[left],nums[right]));
+                    left = increment(left, nums);
+                    right = decrement(right, nums);
+                } else if(nums[left] + nums[right] > required) {
+                    right = decrement(right, nums);
+                } else {
+                    left = increment(left, nums);
+                }
+            }
+
+            base = increment(base, nums);
+        }
+
+        return ans;
+    }
+
+    public int increment(int i, int[] nums) {
+        while (i + 1 < nums.length && nums[i] == nums[i + 1]) i++;
+        return i + 1;
+    }
+
+    public int decrement(int i, int[] nums) {
+        while (i - 1 > 0 && nums[i] == nums[i - 1]) i--;
+        return i - 1;
+    }
+}
