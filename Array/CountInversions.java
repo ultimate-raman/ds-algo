eeeabc58ddde105d2869d58931c4d547644e314f
Raman Ailawadhi <ramanailawadhi@gmail.com>
2025-04-16 02:05:42 +0530

    Count Inversions

---
+++ Array/CountInversions.java
@@ -0,0 +1,51 @@
+package Array;
+
+//Striver
+public class CountInversions {
+    private long ans = 0;
+
+    private void mergeSort(int[] nums, int start, int end) {
+        if(start>=end) return;
+        
+        int mid = (start + end)/2;
+        mergeSort(nums, start, mid);
+        mergeSort(nums, mid+1, end);
+        merge(nums, start, mid, end);
+    }
+
+    private void merge(int[] nums, int start, int mid, int end) {
+        int[] temp = new int[end-start+1];
+
+        int left = start;
+        int right = mid+1;
+
+        int k = 0;
+
+        while(left<=mid && right <=end) {
+            if (nums[left] <= nums[right]) {
+                temp[k++] = nums[left++];
+            } else {
+                ans+= (mid-left+1);
+                temp[k++] = nums[right++];
+            }
+        }
+
+        while(left<=mid){
+            temp[k++] = nums[left++];
+        }
+
+        while(right<=end) {
+            temp[k++] = nums[right++];
+        }
+
+        for (int t = 0; t < k; ++t) {
+            nums[start + t] = temp[t];
+        }
+    }
+
+
+    public long numberOfInversions(int[] nums) {
+        mergeSort(nums,0,nums.length-1);
+        return ans;
+    }
+}
